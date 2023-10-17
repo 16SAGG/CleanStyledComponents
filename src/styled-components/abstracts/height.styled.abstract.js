0ded9d95d29c0b9ddf6dc80fcdfc7a5583e6e9c1
Samuel <sagg160800@gmail.com>
2023-10-17 14:27:05 -0400

    'widthAndHeightAdded'

--- src/styled-components/abstracts/height.styled.abstract.js
+++ src/styled-components/abstracts/height.styled.abstract.js
@@ -1,8 +1,10 @@
 export const height = ({
+    $height = undefined,
     $minHeight = undefined, 
     $maxHeight = undefined,
 }) =>{
     return `
+        ${($height) ? `height : ${$height};` : ''}
         ${($minHeight) ? `min-height : ${$minHeight};` : ''}
         ${($maxHeight) ? `max-height : ${$maxHeight};` : ''}
     `
--- src/styled-components/abstracts/width.styled.abstract.js
+++ src/styled-components/abstracts/width.styled.abstract.js
@@ -1,8 +1,10 @@
 export const width = ({
+    $width = undefined,
     $minWidth = undefined, 
     $maxWidth = undefined,
 }) =>{
     return `
+        ${($width) ? `width : ${$width};` : '' }
         ${($minWidth) ? `min-width : ${$minWidth};` : ''}
         ${($maxWidth) ? `max-width : ${$maxWidth};` : ''}
     `
